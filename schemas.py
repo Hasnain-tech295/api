eca828bbea7b9fcff1cb71b0a745df4d93500f8a
Hasnain-tech295 <hasnainansari2430@gmail.com>
2026-04-18 15:30:18 +0530

    feat: add BookResponse schema and update API endpoints to include year field and response models

--- schemas.py
+++ schemas.py
@@ -1,7 +1,14 @@
 from pydantic import BaseModel, Field
+from datetime import datetime
 
 class BookCreate(BaseModel):
     title: str = Field(min_length=1, max_length=200)
     author: str = Field(min_length=1, max_length=200)
-    year: int = Field(gt=0, le=2026)
+    year: int = Field(gt=0, le=datetime.now().year)
     
+
+class BookResponse(BaseModel):  
+    id: int
+    title: str
+    author: str
+    year: int
